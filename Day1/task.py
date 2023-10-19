8b774289a49c69eac2308a7582002c0076ff1d40
AbdullahAssi <pakabdullah225@gmail.com>
2023-10-20 00:04:34 +0500

    5 Tasks completed

---
+++ Day1/task.py
@@ -0,0 +1,9 @@
+c = int(input("Enter a number: "))
+name = input("Enter your name: ")
+print("Your name is " + name + " and your age is " + str(c))
+
+reversed_str = name[::-1]
+print(reversed_str, " ")
+for i in range(len(name)):
+    print(end=" ")
+    # print(i-1)
